d8da44086142080113c3b07a80f7e7967ffa2a3f
Daniel Scherf <munita.gavai@gmail.com>
2023-10-20 22:34:53 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("java")
+}
+
+group = "moe.das"
+version = "ALPHA"
+
+repositories {
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.13.2-R0.1-SNAPSHOT")
+}
---
+++ src/main/java/moe/das/dasshop/DasShop.java
@@ -0,0 +1,10 @@
+package moe.das.dasshop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DasShop extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Here we go!");
+    }
+}
